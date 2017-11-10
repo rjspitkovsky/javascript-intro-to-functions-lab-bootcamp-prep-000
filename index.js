860d64bcecdb6a02b3edf5752aedbdc94b881bb6
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-10 22:29:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,11 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-`${string}`.toLowerCase === `${string}`
+if (string.toLowerCase() === string) {
+  return "I can't hear you!"; 
+} else if (string.toUpperCase() === string) {
+  return "YES INDEED!";
+} else if (string === "I love you, Grandma.") {
+  return "I love you, too.";
 }
+};
